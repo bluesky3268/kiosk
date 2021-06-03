749ba46d5aac17af337a5e504c35a6325c583198
HYUNBIN_CHO <bluesky3268@gmail.com>
2021-06-03 15:37:14 +0900

    no message

--- src/main/java/korIT/kiosk/dto/MemberLoginDTO.java
+++ src/main/java/korIT/kiosk/dto/MemberLoginDTO.java
@@ -4,10 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 @AllArgsConstructor
 @NoArgsConstructor
